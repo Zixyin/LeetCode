ac449a97309573eba1ed8e515f46dc81ddb8d4c9
YinZiXian <15525112406@163.com>
2020-11-04 08:36:24 +0800

    20201104

--- src/com/yzx/middle/Middle394.java
+++ src/com/yzx/middle/Middle394.java
@@ -1,8 +1,13 @@
 package com.yzx.middle;
 
+import java.util.Scanner;
+
 public class Middle394 {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		String s;
+		while(sc.hasNext() && (s = sc.next())!= "0")
+			System.out.println(s.matches("(\\d)+\\[([a-zA-Z])+\\]"));
 	}
 	class Solution {
 	    public String decodeString(String s) {
